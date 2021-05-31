20c67b99d3bbb9f9ff4f3480d007fba5d3266b9c
Suhas Bhaskar Bhoir <64586841+suhasbhoir@users.noreply.github.com>
2021-05-31 14:41:27 +0530

    New package added

---
+++ Lab/JAVAAssignment/Lab_Assignment_Q1_Activity_i.java
@@ -0,0 +1,21 @@
+package Lab.JAVAAssignment;
+import java.util.Scanner;
+//Q1//i. Write a Java class to swap two numbers without using third variable.
+
+class swap_Two_Number{
+    int x;
+    int y;
+
+
+}
+
+public class Lab_Assignment_Q1_Activity_i {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int x;
+        int y;
+        x = sc.nextInt();
+
+
+    }
+}
